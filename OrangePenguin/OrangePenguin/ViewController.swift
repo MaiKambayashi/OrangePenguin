846719e740d3b2718342277efafe15d010bae2e3
MaiKambayashi <mai.kambayashi@mixi.co.jp>
2015-11-10 19:39:42 +0900

    create OrangePenguin project

---
+++ OrangePenguin/OrangePenguin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OrangePenguin
+//
+//  Created by mai.kambayashi on 2015/11/10.
+//  Copyright © 2015年 mai.kambayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
